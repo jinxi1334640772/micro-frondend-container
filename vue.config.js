199a0de8c626ddddbba10a572bce5967a8cb3fca
zhangjinxi <zhangjinxi@gaodun.com>
2019-08-06 21:39:26 +0800

    xinjian

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  publicPath:'/',//用publishPath代替
+  outputDir:'dist',
+  assetsDir:'assets',
+  lintOnSave:false,
+//   devServer:{
+//     https:false,
+//     host:'localhost',
+//     port:'8080',
+//     open:true,
+//     hotOnly:false,
+//     proxy:{
+//       '/api':{
+//         target:'http//localhost:5000/api/',
+//         ws:true,
+//         changOrigin:true,
+//         pathRewrite:{
+//           '^/api':''
+//         }
+//       }
+//     }
+//   }
+
+
+}
